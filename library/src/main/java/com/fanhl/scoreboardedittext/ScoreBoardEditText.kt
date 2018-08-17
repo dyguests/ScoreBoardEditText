ba398a8aaa58065e82a8f64c083ba29062fdcad1
fanhl <fanhonglin@klicen.com>
2018-08-17 09:21:22 +0800

    Init ScoreBoardEditText

--- library/src/main/java/com/fanhl/scoreboardedittext/ScoreBoardEditText.kt
+++ library/src/main/java/com/fanhl/scoreboardedittext/ScoreBoardEditText.kt
@@ -1,4 +1,14 @@
 package com.fanhl.scoreboardedittext
 
-class ScoreBoardEditText {
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.support.v7.widget.AppCompatEditText
+import android.util.AttributeSet
+
+class ScoreBoardEditText @JvmOverloads constructor(context: Context?, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : AppCompatEditText(context, attrs, defStyleAttr) {
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+    }
 }
